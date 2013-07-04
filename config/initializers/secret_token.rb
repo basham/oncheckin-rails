55741c334562dfc0f1660eed219730686aebeaf6
Chris Basham <chris@bash.am>
2013-07-04 13:32:25 -0400

    Initial creation of Rails 4 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Oncheckin::Application.config.secret_key_base = '4ac4694c54e04ac300b4059f9b4f9c458cfe18888b05a31a425a65a2e574e91acf6319edf0054583dad741637e4d115151516cd2d6949a572fde5811703b97c4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Oncheckin::Application.config.session_store :cookie_store, key: '_oncheckin_session'
